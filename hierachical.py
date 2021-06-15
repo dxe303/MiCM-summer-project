38c28083cbbf5e90fb869d5722d54c446899e9d9
dxe303 <71605598+dxe303@users.noreply.github.com>
2021-06-15 15:21:58 -0400

    Added the clustering analysis code

---
+++ hierachical.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 11:04:50 2021
+
+@author: xueerding
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_excel("/Users/xueerding/Desktop/MiCM/data/Extracted-Parameters.xlsx", 
+                     sheet_name="Combined", header=4, index_col=[0,1], skiprows=[5,])
+
+data_to_clus = data[['Total spikes', 'MFR', 'Number of bursts', 
+                     'Number of network bursts', 'Synchrony index']]
+
+groups = data.index.get_level_values(0)
+print(groups.unique())
+lut = dict(zip(groups.unique(), ['firebrick', 'darkorange', 'gold', 'springgreen', 
+                                 'deepskyblue', 'slateblue', 'purple']))
+print(lut)
+row_colors = groups.map(lut)
+data_clusterGrid = sns.clustermap(data_to_clus, standard_scale=1, figsize=(12, 10), 
+                                  dendrogram_ratio=(.3, .3), cbar_pos=(0, .05, .03, .3), 
+                                  row_colors=row_colors)
+
+
+data2 = pd.read_excel("/Users/xueerding/Desktop/MiCM/data/Extracted-Parameters.xlsx", 
+                     sheet_name="Edited", header=4, index_col=[0,1,2], skiprows=[5,])
+
+print(data2.head(5))
+print(data2.tail(5))
+
+data_to_clus2 = data2[['Total spikes', 'MFR', 'Number of bursts', 
+                     'Number of network bursts', 'Synchrony index']]
+
+groups2 = data2.index.get_level_values(0)
+print(groups2.unique())
+lut2 = dict(zip(groups2.unique(), ['firebrick', 'darkorange', 'gold', 'springgreen', 
+                                   'deepskyblue', 'slateblue', 'purple']))
+print(lut2)
+row_colors2 = groups2.map(lut2)
+data_clusterGrid2 = sns.clustermap(data_to_clus2, standard_scale=1, figsize=(12, 10), 
+                                   dendrogram_ratio=(.3, .3), cbar_pos=(0, .05, .03, .3),
+                                   row_colors=row_colors2 )
---
+++ kmeans.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 11:04:50 2021
+
+@author: xueerding
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+data = pd.read_excel("/Users/xueerding/Desktop/MiCM/data/Extracted-Parameters.xlsx", sheet_name="Combined", header=4, index_col=[0,1], skiprows=[5,])
+
+print(data.head(5))
+print(data.tail(5))
+
+data.describe()
+
+kmeans = KMeans(n_clusters=3, init='k-means++')
+
+
+data['cluster'] = kmeans.fit_predict(data[["Total spikes", "MFR", "Number of bursts", "Number of network bursts", "Synchrony index"]])
+
+print(data.head(5))
+
+# visualize
+centroids = kmeans.cluster_centers_
+cen_x = [i[0] for i in centroids] 
+cen_y = [i[1] for i in centroids]
+
+data['cen_x'] = data.cluster.map({0:cen_x[0], 1:cen_x[1], 2:cen_x[2]})
+data['cen_y'] = data.cluster.map({0:cen_y[0], 1:cen_y[1], 2:cen_y[2]})
+
+colors = ['#DF2020', '#81DF20', '#2095DF']
+data['c'] = data.cluster.map({0:colors[0], 1:colors[1], 2:colors[2]})
+
+plt.scatter(data["Total spikes"], data["Number of bursts"], c=data.c, alpha = 0.6, s=10)
+
+fig = plt.figure(figsize=(26,6))
+ax = fig.add_subplot(131, projection='3d')
+ax.scatter(data["Total spikes"], data["MFR"], data["Synchrony index"], c=data.c, s=15)
+ax.set_xlabel('Total spikes')
+ax.set_ylabel('MFR')
+ax.set_zlabel('Synchrony index')
+plt.show()
